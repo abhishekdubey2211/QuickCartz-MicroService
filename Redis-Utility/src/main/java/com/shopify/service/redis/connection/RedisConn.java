85921f21794a9aa1d5a57d1c395d6ec8d2e5803b
Abhishek Dubey <abdubey42@gmail.com>
2025-02-20 08:10:39 +0530

    redis

--- Redis-Utility/src/main/java/com/shopify/service/redis/connection/RedisConn.java
+++ Redis-Utility/src/main/java/com/shopify/service/redis/connection/RedisConn.java
@@ -1,149 +1,170 @@
 package com.shopify.service.redis.connection;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
-import javax.net.ssl.SSLParameters;
-import javax.net.ssl.SSLSocketFactory;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.*;
-import com.shopify.service.redis.constants.ConstantData;
-import com.shopify.service.redis.model.ServerDetails;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.ExecutionException;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPubSub;
 import redis.clients.jedis.exceptions.JedisException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
-public class RedisConn {
-	private static final Logger log = LoggerFactory.getLogger(RedisConn.class);
-	private JedisSentinelPool objSentinelPool;
-	private JedisPool objSinglePool;
-	private int nType = 0;
-	private ServerDetails objRedisServerDetails = null;
+public class RedisCommonMethods {
+	private static final Logger log = LoggerFactory.getLogger(RedisCommonMethods.class);
+	private final JedisPool jedisPool;
+	private static final int DEFAULT_EXPIRY = (int) TimeUnit.HOURS.toSeconds(24);
+	private static final Gson GSON = new Gson(); // Properly instantiate Gson
 
-//    public RedisConn(ServerDetails redisServerDetails) {
-//        if (redisServerDetails == null) {
-//            throw new IllegalArgumentException("Redis server details cannot be null");
-//        }
-//        this.objRedisServerDetails = redisServerDetails;
-//    }
+	public RedisCommonMethods(JedisPool jedisPool) {
+		this.jedisPool = jedisPool;
+	}
 
-	public void initializeRedis() {
-		log.info("******* Initializing Redis ******* Version: {}", ConstantData.strRedisVersion);
-
-		if (objRedisServerDetails == null) {
-			objRedisServerDetails = new ServerDetails();
-			objRedisServerDetails.setIpAddress("localhost");
-			objRedisServerDetails.setPortno(6379);
-			objRedisServerDetails.setConnectiontype(1);
-		}
-		final Set<String> sentinels = new HashSet<>();
-		nType = objRedisServerDetails.getConnectiontype();
-		JedisPoolConfig objConfig = new JedisPoolConfig();
-		objConfig.setMaxTotal(100);
-		objConfig.setMaxIdle(40);
-		objConfig.setMinIdle(5);
-		objConfig.setTestOnBorrow(true);
-		objConfig.setTestOnCreate(true);
-		objConfig.setTestOnReturn(true);
-		objConfig.setNumTestsPerEvictionRun(-1);
-		int ntimeout = 2000;
-
-		try {
-			switch (nType) {
-			case ConstantData.TYPE_SENTINAL:
-				sentinels.add(objRedisServerDetails.getIpAddress() + ":" + objRedisServerDetails.getPortno());
-				objSentinelPool = new JedisSentinelPool(objRedisServerDetails.getLoginName(), sentinels, objConfig,
-						ntimeout, objRedisServerDetails.getPassword());
-				log.info("Initialized Redis with sentinels.");
-				break;
-			case ConstantData.TYPE_SINGLE:
-				objSinglePool = new JedisPool(objConfig, objRedisServerDetails.getIpAddress(),
-						objRedisServerDetails.getPortno(), ntimeout, objRedisServerDetails.getPassword());
-				log.info("Initialized Redis without sentinels.");
-				break;
-			case ConstantData.TYPE_SINGLE_SECURE:
-				SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-				SSLParameters sslParameters = new SSLParameters();
-				sslParameters.setEndpointIdentificationAlgorithm("HTTPS");
-				log.debug("Configured SSL parameters for secure connection.");
-
-				objSinglePool = new JedisPool(objConfig, objRedisServerDetails.getIpAddress(),
-						objRedisServerDetails.getPortno(), ntimeout, objRedisServerDetails.getPassword(), true);
-				log.info("Initialized Redis with secure connection.");
-				break;
-			default:
-				log.warn("Unknown Redis connection type: {}", nType);
-				return;
-			}
-
-			try (Jedis objJedis = getRedisConn()) {
-				if (objJedis != null) {
-					log.info("Redis connection is alive: {}", objJedis.ping());
-				} else {
-					log.error("Failed to establish Redis connection.");
-				}
-			}
+	public void set(String key, String value, Integer expiry) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			int expireTime = (expiry != null) ? expiry : DEFAULT_EXPIRY;
+			jedis.set(key, value);
+			jedis.expire(key, expireTime);
 		} catch (JedisException e) {
-			log.error("JedisException occurred while initializing Redis: ", e);
+			log.error("Error setting key {} in Redis: ", key, e);
+		}
+	}
+
+	public String get(String key) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			return jedis.get(key);
+		} catch (JedisException e) {
+			log.error("Error getting key {} from Redis: ", key, e);
+			return null;
+		}
+	}
+
+	public void delete(String key) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			jedis.del(key);
+		} catch (JedisException e) {
+			log.error("Error deleting key {} from Redis: ", key, e);
+		}
+	}
+
+	public boolean exists(String key) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			return jedis.exists(key);
+		} catch (JedisException e) {
+			log.error("Error checking existence of key {} in Redis: ", key, e);
+			return false;
+		}
+	}
+
+	public void hset(String key, String field, String value, Integer expiry) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			int expireTime = (expiry != null) ? expiry : DEFAULT_EXPIRY;
+			jedis.hset(key, field, value);
+			jedis.expire(key, expireTime);
+		} catch (JedisException e) {
+			log.error("Error setting hash field {} in key {} in Redis: ", field, key, e);
+		}
+	}
+
+	public String hget(String key, String field) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			return jedis.hget(key, field);
+		} catch (JedisException e) {
+			log.error("Error getting hash field {} from key {} in Redis: ", field, key, e);
+			return null;
+		}
+	}
+
+	public void hdel(String key, String field) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			jedis.hdel(key, field);
+		} catch (JedisException e) {
+			log.error("Error deleting hash field {} from key {} in Redis: ", field, key, e);
+		}
+	}
+
+	public void publish(String channel, String message) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			jedis.publish(channel, message);
+		} catch (JedisException e) {
+			log.error("Error publishing message to channel {} in Redis: ", channel, e);
+		}
+	}
+
+	public void subscribe(String channel, JedisPubSub subscriber) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			jedis.subscribe(subscriber, channel);
+		} catch (JedisException e) {
+			log.error("Error subscribing to channel {} in Redis: ", channel, e);
+		}
+	}
+	public List<String>  getAllChannels() {
+		try (Jedis jedis = jedisPool.getResource()) {
+			return  jedis.pubsubChannels();
+		} catch (JedisException e) {
+			log.error("Error retrieving all Redis pub/sub channels: ", e);
+			return null;
+		}
+	}
+
+
+	public void flushAll() {
+		try (Jedis jedis = jedisPool.getResource()) {
+			jedis.flushAll();
+		} catch (JedisException e) {
+			log.error("Error flushing Redis: {}", e.getMessage(), e);
+		}
+	}
+
+	public void jsonSet(String key, String jsonValue, Integer expiry) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			int expireTime = (expiry != null) ? expiry : DEFAULT_EXPIRY;
+			jedis.set(key, jsonValue); // Simulating RedisJSON functionality
+			jedis.expire(key, expireTime);
+		} catch (JedisException e) {
+			log.error("Error setting JSON value in key {}: ", key, e);
+		}
+	}
+
+	public String jsonGet(String key) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			return jedis.get(key); // Simulating RedisJSON functionality
+		} catch (JedisException e) {
+			log.error("Error getting JSON value from key {}: ", key, e);
+			return null;
+		}
+	}
+
+	public void jsonDel(String key) {
+		try (Jedis jedis = jedisPool.getResource()) {
+			jedis.del(key); // Simulating RedisJSON functionality
+		} catch (JedisException e) {
+			log.error("Error deleting JSON value from key {}: ", key, e);
+		}
+	}
+
+	private static String convertObjToString(Object obj) {
+		return GSON.toJson(obj);
+	}
+
+	public static <T> T convertStringToObj(String jsonString, Class<T> clazz) {
+		try {
+			return GSON.fromJson(jsonString, clazz);
 		} catch (Exception e) {
-			log.error("Unexpected exception occurred while initializing Redis: ", e);
+			log.error("Error converting JSON string to object: {}", e.getMessage(), e);
+			return null;
 		}
 	}
 
-	public Jedis getRedisConn() {
-	    log.info("Redis Version: {}, Connection Type: {}", getRedisHandlerVersion(), nType);
-	    try {
-	        if (nType == ConstantData.TYPE_SENTINAL || nType == ConstantData.TYPE_SENTINAL_SECURE) {
-	            if (objSentinelPool == null) {
-	                log.error("Sentinel Pool is NULL before initialization");
-	                initializeRedis();
-	            }
-	            if (objSentinelPool == null) {
-	                log.error("Sentinel Pool is still NULL after initialization");
-	                return null;
-	            }
-	            return objSentinelPool.getResource();
-	        } else if (nType == ConstantData.TYPE_SINGLE || nType == ConstantData.TYPE_SINGLE_SECURE) {
-	            if (objSinglePool == null) {
-	                log.error("Single Pool is NULL before initialization");
-	                initializeRedis();
-	            }
-	            if (objSinglePool == null) {
-	                log.error("Single Pool is still NULL after initialization");
-	                return null;
-	            }
-	            return objSinglePool.getResource();
-	        }
-	    } catch (JedisException e) {
-	        log.error("Error retrieving Redis connection: ", e);
-	    }
-	    return null;
-	}
-
-	public static String getRedisHandlerVersion() {
-		return "Redis Handler v0.0.1 - 7th February 2025";
-	}
-
-	public void close() {
+	public static <T> T convertStringToObjType(String jsonString, TypeToken<T> typeToken) {
 		try {
-			if (objSentinelPool != null) {
-				objSentinelPool.close();
-				log.info("Closed JedisSentinelPool.");
-			}
-			if (objSinglePool != null) {
-				objSinglePool.close();
-				log.info("Closed JedisPool.");
-			}
-		} catch (JedisException e) {
-			log.error("Error while closing Redis connection pools: ", e);
+			return GSON.fromJson(jsonString, typeToken.getType());
+		} catch (Exception e) {
+			log.error("Error converting JSON string to object type: {}", e.getMessage(), e);
+			return null;
 		}
 	}
 }
